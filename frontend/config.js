00fd16ba23831c9cd14d89628ca6a668db87477b
Carl Arbolado <arbolado.carl.velasquez@gmail.com>
2026-02-13 00:52:52 +0800

    first commit with new added functions

---
+++ frontend/config.js
@@ -0,0 +1,8 @@
+window.APP_CONFIG = {
+  STUDENT_NAME: "Carl", //change this to your name
+  API_BASE_URL: "https://6p8pzsa2ml.execute-api.ap-southeast-1.amazonaws.com/prod", //insert your API Gateway URL here
+
+  TASKS_PATH: "/tasks",
+  TASK_BY_ID_PATH: "/tasks/{id}",
+  CATEGORIES_PATH: "/categories"
+};
